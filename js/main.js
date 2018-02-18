c62436692a71ba3ffb7c66d557c743190d44fddf
Sári Csaba <csabi.sari@gmail.com>
2018-02-18 22:45:04 +0100

    Feat: függvények egyesítése

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Callback függvény fade után.
+
 function fadeDone() {
     console.log(this);
 }
@@ -7,13 +8,14 @@ function fadeDone() {
 $("p").click(function(){
     $(this).hide();
     //$(this).fadeTo(2000,1,fadeDone);
-    $(this).slideDown(1500);
+    //$(this).slideDown(1500);
 });
 
 // Esemény kiváltása
-$("p").click();
+// $("p").click();
 
 // Kattintás megelőzése.
+/*
 $("nav a.nav-link").click(function(ev) {
     event.preventDefault();
     var link = $(this);
@@ -22,7 +24,31 @@ $("nav a.nav-link").click(function(ev) {
     var speed = link.data("speed") || 1000;
     var settings = {};
     settings [prop] = val;
+
     $(document.body).animate(settings, speed, function() {
         document.location = link.attr("href");
     })
+});
+
+*/
+
+// Events olda
+
+$(".events-search-row input").on("keyup", function(ev) {
+    $.each( $(".events-card-deck .card .card-title"), function(index, elem)   {
+        var elem = $(elem);
+        var search = ev.target.value.toLowerCase();
+        var content = elem.html().toLowerCase();
+        if ( content.indexOf(search) == -1 ) {
+          elem.parents(".card").hide();
+        } else {
+          elem.parents(".card").show();
+        }
+      });
+    });
+
+// Ticket oldal
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
 });
