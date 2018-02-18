01a8b775462963b66754d44459f4f723421d2097
Sári Csaba <csabi.sari@gmail.com>
2018-02-18 19:21:30 +0100

    Feat: animations added to event and index page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Callback függvény fade után.
+function fadeDone() {
+    console.log(this);
+}
+
+// Eseménykezelő beállítása
 $("p").click(function(){
     $(this).hide();
+    //$(this).fadeTo(2000,1,fadeDone);
+    $(this).slideDown(1500);
+});
+
+// Esemény kiváltása
+$("p").click();
+
+// Kattintás megelőzése.
+$("nav a.nav-link").click(function(ev) {
+    event.preventDefault();
+    var link = $(this);
+    $(document.body).animate( {
+        opacity: '0'
+    }, 1000, function() {
+        document.location = link.attr("href");
+    })
 });
